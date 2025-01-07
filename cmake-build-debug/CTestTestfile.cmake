c7ea84853ac32abb1c34aaef85db8896e8aa38e8
Slipchenko, Lyudmila V <lslipchenko@gmail.com>
2025-01-06 21:09:50 -0500

    final fixes

--- cmake-build-debug/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/lyuda/LIBEFP/libefp_torch_Jan5/libefp
-# Build directory: /Users/lyuda/LIBEFP/libefp_torch_Jan5/libefp/cmake-build-debug
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("efpmd")
-subdirs("tests")
